d80c6c2938f03bea17f93c70dad38d875dee72de
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-01-31 15:43:32 +0100

    fase 2

---
+++ hobbies.js
@@ -0,0 +1,7 @@
+// Creo la funzione
+function Hobbies (stringhe) {
+    return ['hobbyOne', 'hobbyTwo', 'hobbyThree']
+}
+
+// Esporto la funzione 
+module.exports = Hobbies;
---
+++ names.js
@@ -0,0 +1,10 @@
+// Creo la funzione
+function Identificativo(firstName, lastName) {
+    return {
+        firstName: Lorenzo,
+        lastName: Rampa
+    };
+}
+
+// Esporto la funzione
+module.exports = Identificativo;
